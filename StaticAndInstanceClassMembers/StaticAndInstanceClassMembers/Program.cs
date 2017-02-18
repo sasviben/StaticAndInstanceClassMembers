83b718a2f1343508588cfcd27936ddc7965043cb
sandro sviben <sandro.sviben@hotmail.com>
2017-02-18 19:12:11 +0100

    Program is running

--- StaticAndInstanceClassMembers/StaticAndInstanceClassMembers/Program.cs
+++ StaticAndInstanceClassMembers/StaticAndInstanceClassMembers/Program.cs
@@ -8,16 +8,28 @@ namespace StaticAndInstanceClassMembers
 {
     class Circle
     {
+        int radius;
         //staticka varijabla --> kod stvaranja novih objekata ne duplicira se varijabla već svi objekti pokazuju na ovu varijablu u memoriji
-
+        static float _PI;
         
         //privatni konstruktor za inicijalizaciju staticke varijable
-
+        static Circle()
+        {
+            _PI = 3.14F; 
+        }
 
         //konstruktor za inicijalizaciju 
-
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
 
         //metoda koja racuna opseg kruznice
+
+        public float CalculateArea()
+        {
+            return _PI * this.radius * this.radius;
+        }
 
     }
 
@@ -27,8 +39,15 @@ namespace StaticAndInstanceClassMembers
         static void Main(string[] args)
         {
             //prvi objekt klase Circle
+            Circle C1 = new Circle(5);
+            float area1 = C1.CalculateArea();
+            Console.WriteLine("Area = {0}", area1);
 
             //drugi objekt klase circle
+            Circle C2 = new Circle(6);
+            float area2 = C2.CalculateArea();
+            Console.WriteLine("Area = {0}", area2);
+
         }
     }
 }
